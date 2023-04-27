41efdcfb282d7c2bc286698b3201201f0a61e3b0
islamka2 <islamkaawad@gmail.com>
2023-04-27 13:09:28 +0600

    first

---
+++ script/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  $(".btn1").click(function () {
+    $(".sun_info").toggleClass("hidden");
+  });
+  $(".close1").click(function () {
+    $(".sun_info").toggleClass("hidden");
+  });
+
+  $(".btn2").click(function () {
+    $(".mercury_info").toggleClass("hidden");
+  });
+  $(".close2").click(function () {
+    $(".mercury_info").toggleClass("hidden");
+  });
+
+  $(".btn3").click(function () {
+    $(".venus_info").toggleClass("hidden");
+  });
+  $(".close3").click(function () {
+    $(".venus_info").toggleClass("hidden");
+  });
+
+  $(".btn4").click(function () {
+    $(".earth_info").toggleClass("hidden");
+  });
+  $(".close4").click(function () {
+    $(".earth_info").toggleClass("hidden");
+  });
+
+  $(".btn5").click(function () {
+    $(".mars_info").toggleClass("hidden");
+  });
+  $(".close5").click(function () {
+    $(".mars_info").toggleClass("hidden");
+  });
+
+  $(".btn6").click(function () {
+    $(".jupiter_info").toggleClass("hidden");
+  });
+  $(".close6").click(function () {
+    $(".jupiter_info").toggleClass("hidden");
+  });
+
+  $(".btn7").click(function () {
+    $(".saturn_info").toggleClass("hidden");
+  });
+  $(".close7").click(function () {
+    $(".saturn_info").toggleClass("hidden");
+  });
+
+  $(".btn8").click(function () {
+    $(".uranus_info").toggleClass("hidden");
+  });
+  $(".close8").click(function () {
+    $(".uranus_info").toggleClass("hidden");
+  });
+
+  $(".btn9").click(function () {
+    $(".neptune_info").toggleClass("hidden");
+  });
+  $(".close9").click(function () {
+    $(".neptune_info").toggleClass("hidden");
+  });
+});
